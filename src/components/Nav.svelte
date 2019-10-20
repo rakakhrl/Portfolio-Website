7ccbb1e4d906f8475fac68baefdc9131d7ff677e
Raka Khairil <raka.khairil@gmail.com>
2019-10-20 15:08:41 +0700

    initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+</style>
+
+<nav>
+	<a class='{segment === undefined ? "selected" : ""}' href='.'>home</a>
+	<a class='{segment === "about" ? "selected" : ""}' href='about'>about</a>
+	<a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a>
+</nav>
